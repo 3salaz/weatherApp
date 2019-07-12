e88557ec981aeff0a5248b32dd8a8c575b7fcbb7
3salaz <3salaz.dev@gmail.com>
2019-07-12 16:57:02 -0700

    fetched api call for tempurature degrees and summary displayed in api

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 console.log('all-good')
 window.addEventListener('load',() => {
     let long;
-    let lang;
+    let lat;
+    let temperatureDescription = document.querySelector(`.temperature-description`);
+    let temperatureDegree = document.querySelector(`.temperature-degree`);
+
+
+
+    // 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
             long = position.coords.longitude;
-            lang = position.coords.latitude;
+            lat = position.coords.latitude;
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/1dcc572c7fc8c24f63d85e82150a216c/${lat},${long}`;
+            fetch(api)
+            .then(response => {
+                // convert the response from the api in json
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const { temperature,summary } = data.currently;
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+            });
         });
     };
 });
